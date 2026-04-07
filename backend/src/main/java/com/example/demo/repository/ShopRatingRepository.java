42563dd2527ceaa0a1fd41ae5a548dd961f163b3
xback <2327722452@qq.com>
2026-04-08 02:56:02 +0800

    删除非必要文件

--- backend/src/main/java/com/example/demo/repository/ShopRatingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ShopRating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRatingRepository extends JpaRepository<ShopRating, Long> {
-    ShopRating findByShopId(Long shopId);
-}
